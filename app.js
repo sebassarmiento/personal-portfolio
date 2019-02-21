21957b17f007cc45090f3ff7ff9f5c137cff53c9
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2019-02-21 16:27:17 -0500

    Background is gradient from top to bottom

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 console.log('Init app');
 
-$(window).scroll(() => {
-    let scrollTop = $(window).scrollTop()
-    if(scrollTop > 0){
-        $('.navbar').addClass('scrolled')
-    } else {
-        $('.navbar').removeClass('scrolled')
+$(window).on('scroll', function () {
+    let windowHeight = $(window).height()
+
+    let pixs = $(document).scrollTop()
+    console.log('scroll', pixs)
+    if(pixs < 460){
+        pixs = pixs / 70;
+        $(".text").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" }) 
     }
-})
+
+    console.log('HEIGHT', windowHeight)
+    
+    if(pixs > (windowHeight + 94)){
+        $('.portfolio-title').css({ "font-size": '16px' })
+    } else {
+        $('.portfolio-title').css({ "font-size": '2em' })
+    }
+});
