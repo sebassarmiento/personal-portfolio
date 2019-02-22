cabb19c8cf0d98859b1625c0cdc253bd43801c3a
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2019-02-22 13:10:34 -0500

    Working progress on personal portfolio.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 console.log('Init app');
 
+
+
 $(window).on('scroll', function () {
     let windowHeight = $(window).height()
 
     let pixs = $(document).scrollTop()
     console.log('scroll', pixs)
     if(pixs < 460){
-        pixs = pixs / 70;
+        pixs = pixs / 20;
         $(".text").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)" }) 
     }
 
@@ -17,4 +19,24 @@ $(window).on('scroll', function () {
     } else {
         $('.portfolio-title').css({ "font-size": '2em' })
     }
+
+    var maxScroll = $("body")[0].scrollHeight;
+
+    if(pixs > maxScroll - 730){
+        $('.contact-title').css({ "font-size": '16px' })
+    } else {
+        $('.contact-title').css({ "font-size": '2em' })
+    }
+});
+
+
+
+$(".see-portfolio").click(() => {
+    console.log('click')
+    $('.see-portfolio').addClass('clicked')
+    $('html,body').animate({
+        scrollTop: $(".portfolio").offset().top - 150},
+        {
+            duration: 1000
+        });
 });
